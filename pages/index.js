078fe4646ecc2ac277c9470ae3f37680b4457dc2
mackbowes <mackenziepbowes@gmail.com>
2021-01-01 17:04:13 -0800

    fixing links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Nav from '../components/nav'
-import Resume from '../components/resume'
 import Button from '../components/Button.js';
 import { motion } from "framer-motion"
 import styles from '../styles/Home.module.css'

e6221988362d9ee3f5210279610c421bc5fd1619
mackbowes <mackenziepbowes@gmail.com>
2021-06-02 14:03:41 -0700

    updated and expanded product generator

--- pages/things/productgenerator.js
+++ pages/things/productgenerator.js
@@ -59,7 +59,24 @@ export default function ThisPage() {
 					<Section>
 						<h2 id={'Product'}>Your Product</h2>
 						<p>
-							Your destiny is calling. You will unlock untold riches and become a true sigma boss by creating a business that sells <b>{product}</b> to people who value <b>{value}</b>, love the <a href={aesthetic.link}><b>{aesthetic.name}</b></a> aesthetic, and prefer the color <b>{color}</b>.
+							Your destiny is calling. You will unlock untold riches by creating a business that sells <b>{product}</b> to people who value <b>{value}</b>, love the <a href={aesthetic.link} target="_blank"><b>{aesthetic.name}</b></a> aesthetic, and prefer the color <b>{color}</b>.
+						</p>
+						<h2>Your Results Explained</h2>
+						<h3>Your Product is <b>{product}</b></h3>
+						<p>
+							What the product is doesn't really matter. I chose basic commodities that people can incorporate into their identity. Sometimes it's a one time purchase, sometimes it's something that someone has to buy every month or quarter. If you get a one time purchase thing, that's totally fine because you can build an audience profitably and sell the other products to them.
+						</p>
+						<h3>Your audience values <b>{value}</b></h3>
+						<p>
+							Your audience's value informs your brands images, photography style, design style, and so on. It also informs your copy - focus on your audiences value on each touch point, relate every post and ad back to {value}, and your audience will feel totally understood by you.
+						</p>
+						<h3>Your audience loves the <b>{aesthetic.name}</b> aesthetic</h3>
+						<p>
+							People strive to be part of a group. Use that. Make purchasing your product put them in with the 'in-crowd' of this aesthetic, giving them a sense of status and connection to a community they admire but don't quite feel like they're a part of (hint: no one really feels welcome in any aesthetic group).
+						</p>
+						<h3>Your audience prefers <b>{color}</b></h3>
+						<p>
+							Maybe they don't. Don't get hung up on this color, or assumy that they will betray your brand if you don't use this color. There are other colors, especially if your aesthetic is color specific rather than ethos specific or multi-colored. Use the color preference to choose a direction with audiences whose values and aesthetic don't demand a specific palette.
 						</p>
 					</Section>
 				</Block>
